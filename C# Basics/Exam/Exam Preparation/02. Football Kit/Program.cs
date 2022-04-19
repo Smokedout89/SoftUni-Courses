7e8ab1567ca4b3f98a962435c55f41ad30533e95
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-04-19 11:23:53 +0300

    Uploading new files

---
+++ C# Basics/Exam/Exam Preparation/02. Football Kit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Football_Kit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shirtPrice = double.Parse(Console.ReadLine());
+            double moneyToReach = double.Parse(Console.ReadLine());
+
+            double shortPrice = shirtPrice * 0.75;
+            double socksPrice = shortPrice * 0.20;
+            double buttonsPrice = (shortPrice + shirtPrice) * 2;
+
+            double totalPriceAfterDiscount = (shirtPrice + shortPrice + socksPrice + buttonsPrice) * 0.85;
+
+            if (totalPriceAfterDiscount >= moneyToReach)
+            {
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {totalPriceAfterDiscount:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {moneyToReach - totalPriceAfterDiscount:f2} lv. more.");
+            }
+           
+        }
+    }
+}
---
+++ C# Basics/Exam/Exam Preparation/03. Excursion Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _03._Excursion_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double pricePerPerson = 0;
+
+            if (numberOfPeople <= 5)
+            {
+                switch (season)
+                {
+                    case "spring":
+                        pricePerPerson = 50.00;
+                        break;
+                    case "summer":
+                        pricePerPerson = 48.50;
+                        break;
+                    case "autumn":
+                        pricePerPerson = 60.00;
+                        break;
+                    case "winter":
+                        pricePerPerson = 86.00;
+                        break;
+                }
+            }
+            else
+            {
+                switch (season)
+                {
+                    case "spring":
+                        pricePerPerson = 48.00;
+                        break;
+                    case "summer":
+                        pricePerPerson = 45.00;
+                        break;
+                    case "autumn":
+                        pricePerPerson = 49.50;
+                        break;
+                    case "winter":
+                        pricePerPerson = 85.00;
+                        break;
+                }
+            }
+
+            double price = numberOfPeople * pricePerPerson;
+            if (season == "summer")
+            {
+                price *= 0.85;
+            }
+            else if (season == "winter")
+            {
+                price *= 1.08;
+            }
+
+            Console.WriteLine($"{price:f2} leva.");
+
+        }
+    }
+}

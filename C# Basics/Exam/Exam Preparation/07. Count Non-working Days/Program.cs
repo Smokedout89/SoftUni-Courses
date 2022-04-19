12bc7ba572150eead2bd430322b129c2ce79635a
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-04-19 11:27:03 +0300

    Uploading new files

---
+++ C# Basics/Exam/Exam Preparation/07. Count Non-working Days/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                     date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+            }
+
+            Console.WriteLine(holidaysCount);
+
+
+        }
+    }
+}

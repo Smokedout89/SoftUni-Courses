4a741cb3aecd43c14226feac513b7010c960535d
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-04-19 11:31:09 +0300

    Upload new files

---
+++ C# Basics/Exam/Exam - Basic/05. Puppy Care/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodInKg = int.Parse(Console.ReadLine());
+            int foodInGrams = foodInKg * 1000;
+
+            string input = Console.ReadLine();
+
+            int totalFoodEaten = 0;
+
+            while (input != "Adopted")
+            {
+                int foodEaten = int.Parse(input);
+                totalFoodEaten += foodEaten;
+
+                input = Console.ReadLine();
+            }
+
+            if (foodInGrams >= totalFoodEaten)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodInGrams-totalFoodEaten} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {totalFoodEaten-foodInGrams} grams more.");
+            }
+        }
+    }
+}
---
+++ C# Basics/Exam/Exam - Basic/06. Gold Mine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLocations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfLocations; i++)
+            {
+                double expectedAverageIncome = double.Parse(Console.ReadLine());
+                int numOfDays = int.Parse(Console.ReadLine());
+                double averageGoldMined = 0;
+
+                for (int j = 0; j < numOfDays; j++)
+                {
+                    double goldMined = double.Parse(Console.ReadLine());
+                    averageGoldMined += goldMined;
+                }
+
+                double averageDailyMined = averageGoldMined / numOfDays;
+
+                if (averageDailyMined >= expectedAverageIncome)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {averageDailyMined:f2}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You need {expectedAverageIncome-averageDailyMined:f2} gold.");
+                }
+
+            }
+
+        }
+    }
+}

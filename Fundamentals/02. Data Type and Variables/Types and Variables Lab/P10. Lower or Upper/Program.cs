5ccbee695fd61fdfebbeacba371ded2f30317047
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-02 20:59:27 +0300

    Uploading new files

---
+++ Fundamentals/02. Data Type and Variables/Types and Variables Lab/P10. Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (char.IsLower(letter))
+            {
+                Console.WriteLine("lower-case");
+            }
+
+            if (char.IsUpper(letter))
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
---
+++ Fundamentals/02. Data Type and Variables/Types and Variables Lab/P12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int sum = 0;
+                int currentNum = i;
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecial);
+            }
+        }
+    }
+}

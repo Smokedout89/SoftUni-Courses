3f87d207b20b035fe2d6906f32d8997048a0c347
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-09 12:59:37 +0300

    Uploading new files

---
+++ Fundamentals/03. Arrays/Arrays Lab/P05. Sum Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = 0;
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

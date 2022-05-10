d015d1ec3bcd30244e3d3d64073310aafe77e2b2
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-10 12:26:58 +0300

    Uploading new files

---
+++ Fundamentals/03. Arrays/Arrays Lab/P06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace P06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    evenSum += item;
+                }
+                else
+                {
+                    oddSum += item;
+                }
+            }
+
+            int difference = (evenSum - oddSum);
+            Console.WriteLine(difference);
+        }
+    }
+}
---
+++ Fundamentals/03. Arrays/Arrays Lab/P07. Equal Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace P07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sum = 0;
+            bool isIdentical = true;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                sum += arr2[i];
+
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isIdentical = false;
+                    break;
+                }
+
+            }
+
+            if (isIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}

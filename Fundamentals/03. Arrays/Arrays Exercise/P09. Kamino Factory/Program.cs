b0f8c4ae4cc36e8eb7fe7d9908b0486b89a01d0e
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-14 11:24:29 +0300

    Uploading new files

---
+++ Fundamentals/03. Arrays/Arrays Exercise/P09. Kamino Factory/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace P09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceLength = int.Parse(Console.ReadLine());
+            int[] bestDNA = new int[sequenceLength];
+
+            int dnaSum = 0;
+            int dnaCount = -1;
+            int dnaStartIndex = -1;
+            int dnaSample = 0;
+
+            int sample = 0;
+            string input;
+
+            while ((input = Console.ReadLine()) != "Clone them!")
+            {
+                sample++;
+
+                int[] currentDNA = input.Split('!', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                int currentLengthCount = 0;
+                int currentDNASum = 0;
+                int currentStartIndex = 0;
+                int currentEndIndex = 0;
+                bool isCurrentDNABetter = false;
+
+                int lengthCount = 0;
+
+                for (int i = 0; i < currentDNA.Length; i++)
+                {
+                    if (currentDNA[i] != 1)
+                    {
+                        lengthCount = 0;
+                        continue;
+                    }
+
+                    lengthCount++;
+
+                    if (lengthCount > currentLengthCount)
+                    {
+                        currentLengthCount = lengthCount;
+                        currentEndIndex = i;
+                    }
+                }
+
+                currentStartIndex = currentEndIndex - currentLengthCount + 1;
+                currentDNASum = currentDNA.Sum();
+
+                if (currentLengthCount > dnaCount)
+                {
+                    isCurrentDNABetter = true;
+                }
+                else if (currentLengthCount == dnaCount)
+                {
+                    if (currentStartIndex < dnaStartIndex)
+                    {
+                        isCurrentDNABetter = true;
+                    }
+                    else if (currentStartIndex == dnaStartIndex)
+                    {
+                        if (currentDNASum > dnaSum)
+                        {
+                            isCurrentDNABetter = true;
+                        }
+                    }
+                }
+
+                if (isCurrentDNABetter)
+                {
+                    bestDNA = currentDNA;
+                    dnaCount = currentLengthCount;
+                    dnaStartIndex = currentStartIndex;
+                    dnaSum = currentDNASum;
+                    dnaSample = sample;
+                }
+
+            }
+
+            Console.WriteLine($"Best DNA sample {dnaSample} with sum: {dnaSum}.");
+            Console.WriteLine(String.Join(' ', bestDNA));
+        }
+    }
+}
---
+++ Fundamentals/03. Arrays/Arrays Exercise/P10. LadyBugs/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace P10._LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] ladybugIndexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (ladybugIndexes.Contains(i))
+                {
+                    field[i] = 1;
+                }
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandArguments = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                int initialIndex = (int.Parse(commandArguments[0]));
+                string direction = commandArguments[1];
+                int flyLength = int.Parse(commandArguments[2]);
+
+                if (initialIndex < 0 || initialIndex >= field.Length)
+                {
+                    continue;    // Check if index is inside the field, if not nothing happen, skips the command
+                }
+
+                if (field[initialIndex] == 0)
+                {
+                    continue;  // Check if there is a bug at this index, if not nothing happen, skips the command
+                }
+
+                field[initialIndex] = 0;   // The bug start flying, its index goes to 0
+                int nextIndex = initialIndex;  // initialize next index 
+
+                while (true)
+                {
+                    if (direction == "right")
+                    {
+                        nextIndex += flyLength; // calculate next index
+                    }
+                    else if (direction == "left")
+                    {
+                        nextIndex -= flyLength; // calculate next index
+                    }
+
+                    if (nextIndex < 0 || nextIndex >= field.Length)
+                    {
+                        break; // Check if next index is valid, if not ladybug is gone into the void
+                    }
+
+                    if (field[nextIndex] == 0)
+                    {
+                        break; // Check if index is free to land or there is another ladybug there
+                               // If there is already bug, we break, and repeat the same flylength
+                    }
+                }
+
+                if (nextIndex >= 0 && nextIndex < field.Length)
+                {
+                    field[nextIndex] = 1;
+                }
+
+            }
+
+            Console.WriteLine(String.Join(' ', field));
+        }
+    }
+}

143d41f5d2f0cd2c300e3a909cc9ec3ecaa35990
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-17 19:54:31 +0300

    Uploading new files

---
+++ Fundamentals/03. Arrays/Arrays More Exercise/P04. Fold and Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int k = inputArray.Length / 4;
+
+            int[] newArr = new int[k * 2];
+
+            for (int i = 0; i < k; i++)
+            {
+                newArr[i] = inputArray[k - (i + 1)] + inputArray[k + i];
+                newArr[newArr.Length - 1 - i] = inputArray[newArr.Length - 1 - i + k] +
+                                                inputArray[(newArr.Length - 1 - i) + (k + 2 * i + 1)];
+            }
+
+            Console.WriteLine(string.Join(' ', newArr));
+        }
+    }
+}
---
+++ Fundamentals/03. Arrays/Arrays More Exercise/P05. Longest Increasing Subsequence (LIS)/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05._Longest_Increasing_Subsequence__LIS_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] solutions = new int[numbers.Length];
+            int[] previous = new int[numbers.Length];
+
+            int maxSolution = 0;
+            int maxSolutionIndex = 0;
+
+            for (int current = 0; current < numbers.Length; current++)
+            {
+                int solution = 1;
+                int previousIndex = -1;
+                int currentNumber = numbers[current];
+
+                for (int solutionIndex = 0; solutionIndex < current; solutionIndex++)
+                {
+                    int previousNumber = numbers[solutionIndex];
+                    int previousSolution = solutions[solutionIndex];
+
+                    if (currentNumber > previousNumber && solution <= previousSolution)
+                    {
+                        solution = previousSolution + 1;
+                        previousIndex = solutionIndex;
+                    }
+                }
+
+                solutions[current] = solution;
+                previous[current] = previousIndex;
+
+                if (solution > maxSolution)
+                {
+                    maxSolution = solution;
+                    maxSolutionIndex = current;
+                }
+            }
+
+            int index = maxSolutionIndex;
+            List<int> result = new List<int>();
+
+            while (index != -1)
+            {
+                int currentNumbers = numbers[index];
+                result.Add(currentNumbers);
+                index = previous[index];
+            }
+
+            result.Reverse();
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}

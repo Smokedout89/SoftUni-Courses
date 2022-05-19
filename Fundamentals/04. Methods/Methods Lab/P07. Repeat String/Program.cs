15ee6d810e2b19e16fdd385e80f43ade2c3a030e
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-19 10:59:34 +0300

    Uploading new files

---
+++ Fundamentals/04. Methods/Methods Lab/P07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace P07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeats = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatedString(input, repeats));
+        }
+
+        private static string RepeatedString(string input, int repeats)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < repeats; i++)
+            {
+                result.Append(input);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Fundamentals/04. Methods/Methods Lab/P08. Math Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Powered(baseNum, power));
+        }
+
+        static double Powered(double baseNum, int power)
+        {
+            double result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+    }
+}

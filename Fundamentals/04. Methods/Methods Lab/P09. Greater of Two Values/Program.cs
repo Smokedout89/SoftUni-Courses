5311196e2212dc5fa8bb0482bf43e3752640d99d
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-19 10:59:58 +0300

    Uploading new files

---
+++ Fundamentals/04. Methods/Methods Lab/P09. Greater of Two Values/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace P09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "int":
+                    int num1 = int.Parse(Console.ReadLine());
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(num1, num2));
+                    
+                    break;
+                case "char":
+                    char char1 = char.Parse(Console.ReadLine());
+                    char char2 = char.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(char1, char2));
+                    break;
+                case "string":
+                    string string1 = Console.ReadLine();
+                    string string2 = Console.ReadLine();
+                    Console.WriteLine(GetMax(string1, string2));
+                    break;
+            }
+            
+        }
+
+        static int GetMax(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+
+        static char GetMax(char a, char b)
+        {
+           
+            return (char) Math.Max(a, b);
+        }
+
+        static string GetMax(string a, string b)
+        {
+            int comparison = a.CompareTo(b);
+
+            if (comparison > 0)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+    }
+}

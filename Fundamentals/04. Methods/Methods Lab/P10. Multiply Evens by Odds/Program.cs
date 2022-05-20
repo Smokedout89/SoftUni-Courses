87c3074592168dc72885825a5d48f3ce52fc00ea
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-20 11:21:07 +0300

    Uploading new files

---
+++ Fundamentals/04. Methods/Methods Lab/P10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace P10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int evenSum = GetSumOfEvenDigits(number);
+            int oddSum = GetSumOfOddDigits(number);
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds(evenSum, oddSum));
+
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int currDigit = number % 10;
+
+                if (currDigit % 2 == 0)
+                {
+                    sum += currDigit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int currDig = number % 10;
+
+                if (currDig % 2 != 0)
+                {
+                    sum += currDig;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
---
+++ Fundamentals/04. Methods/Methods Lab/P11. Math Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P11._Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(numberOne, @operator, numberTwo));
+        }
+
+        static double Calculate(int num1, string input, int num2)
+        {
+            double result = 0;
+
+            switch (input)
+            {
+                case "+": result = num1 + num2; break;
+                case "-": result = num1 - num2; break;
+                case "*": result = num1 * num2; break;
+                case "/": result = num1 / num2; break;
+            }
+
+            return result;
+        }
+    }
+}

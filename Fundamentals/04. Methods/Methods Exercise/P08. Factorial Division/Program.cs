274cc87465b7aedc9def942423f9b155e5efe8c7
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-25 10:03:19 +0300

    Uploading new files

---
+++ Fundamentals/04. Methods/Methods Exercise/P08. Factorial Division/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace P08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            double firstFact = Factorial(firstNum);
+            double secondFact = Factorial(secondNum);
+
+            double result = firstFact / secondFact;
+
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double Factorial(int number)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Fundamentals/04. Methods/Methods Exercise/P09. Palindrome Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                bool result = PalindromeIntegerChecker(input);
+                Console.WriteLine(result.ToString().ToLower());
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool PalindromeIntegerChecker(string number)
+        {
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                if (number[i] != number[number.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

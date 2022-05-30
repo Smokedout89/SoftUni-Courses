cdc6fc0e7af6d31cf164eb983d41c2028457a5ea
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-05-30 15:28:50 +0300

    Uploading new files

---
+++ Fundamentals/04. Methods/Methods More Exercise/P04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P04._Tribonacci_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            PrintTribonacci(num);
+        }
+
+        static void PrintTribonacci(int num)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 1;
+            int d = a + b + c;
+
+            Console.Write($"1 ");
+
+            for (int i = 1; i < num; i++)
+            {
+                Console.Write($"{d} ");
+                a = b;
+                b = c;
+                c = d;
+                d = a + b + c;
+            }
+        }
+    }
+}
---
+++ Fundamentals/04. Methods/Methods More Exercise/P05. Multiplication Sign/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P05._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            MultiplicationSign(num1, num2, num3);
+        }
+
+        static void MultiplicationSign(int num1, int num2, int num3)
+        {
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (num1 < 0 && num2 < 0 && num3 > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 < 0 && num2 > 0 && num3 < 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 > 0 && num2 < 0 && num3 < 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 < 0 || num2 < 0 || num3 < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}

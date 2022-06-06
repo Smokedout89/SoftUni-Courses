691022d3a4ecf8864b22717e9586f92819c570fc
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-06 16:46:47 +0300

    Uploading new files

---
+++ Fundamentals/05. Lists/Lists Exercise/P09. Pokemon Don’t Go/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int sumOfRemovedItems = 0;
+
+            while (numbers.Count != 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                if (index < 0)
+                {
+                    index = 0;
+                    int numAtIndex = numbers[index];
+
+                    numAtIndex = numbers[index];
+                    sumOfRemovedItems += numAtIndex;
+                    CatchPokemon(numbers, index, numAtIndex);
+                    int numToCopy = numbers[numbers.Count - 1];
+                    numbers.Insert(index, numToCopy);
+                }
+                else if (index >= numbers.Count)
+                {
+                    index = numbers.Count - 1;
+                    int numAtIndex = numbers[index];
+
+                    numAtIndex = numbers[index];
+                    sumOfRemovedItems += numAtIndex;
+                    CatchPokemon(numbers, index, numAtIndex);
+                    int numToCopy = numbers[0];
+                    numbers.Insert(index, numToCopy);
+                }
+                else
+                {
+                    int numAtIndex = numbers[index];
+                    sumOfRemovedItems += numAtIndex;
+                    CatchPokemon(numbers, index, numAtIndex);
+                }
+
+            }
+
+            Console.WriteLine(sumOfRemovedItems);
+        }
+
+        static void CatchPokemon(List<int> nums, int index, int numAtIndex)
+        {
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] <= numAtIndex)
+                {
+                    nums[i] += numAtIndex;
+                }
+                else
+                {
+                    nums[i] -= numAtIndex;
+                }
+            }
+
+            nums.RemoveAt(index);
+        }
+    }
+}
---
+++ Fundamentals/05. Lists/Lists Exercise/P10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P10._SoftUni_Course_Planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> initialLessons = Console.ReadLine().Split(", ").ToList();
+            string[] cmdArgs = Console.ReadLine().Split(':').ToArray();
+
+            while (cmdArgs[0] != "course start")
+            {
+                string command = cmdArgs[0];
+                string lessonTitle = cmdArgs[1];
+
+                if (command == "Add")
+                {
+                    if (!initialLessons.Contains(lessonTitle))
+                    {
+                        initialLessons.Add(lessonTitle);
+                    }
+                }
+                else if (command == "Insert")
+                {
+                    int indexToInsert = int.Parse(cmdArgs[2]);
+
+                    if (!initialLessons.Contains(lessonTitle))
+                    {
+
+                        if (indexToInsert < initialLessons.Count && indexToInsert >= 0)
+                        {
+                            initialLessons.Insert(indexToInsert, lessonTitle);
+                        }
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    initialLessons.Remove(lessonTitle);
+                    initialLessons.Remove($"{lessonTitle}-Exercise");
+                }
+                else if (command == "Swap")
+                {
+                    string lessonToSwapWith = cmdArgs[2];
+                    int firstLessonIndex = initialLessons.IndexOf(lessonTitle);
+                    int secondLessonIndex = initialLessons.IndexOf(lessonToSwapWith);
+
+                    if (firstLessonIndex != -1 && secondLessonIndex != -1)
+                    {
+                        initialLessons[firstLessonIndex] = lessonToSwapWith;
+                        initialLessons[secondLessonIndex] = lessonTitle;
+
+                        if (firstLessonIndex + 1 < initialLessons.Count && initialLessons[firstLessonIndex + 1] == $"{lessonTitle}-Exercise")
+                        {
+                            initialLessons.RemoveAt(firstLessonIndex + 1);
+                            firstLessonIndex = initialLessons.IndexOf(lessonTitle);
+                            initialLessons.Insert(firstLessonIndex + 1, $"{lessonTitle}-Exercise");
+                        }
+
+                        if (secondLessonIndex + 1 < initialLessons.Count && initialLessons[secondLessonIndex + 1] == $"{lessonToSwapWith}-Exercise")
+                        {
+                            initialLessons.RemoveAt(secondLessonIndex + 1);
+                            secondLessonIndex = initialLessons.IndexOf(lessonToSwapWith);
+                            initialLessons.Insert(secondLessonIndex + 1, $"{lessonToSwapWith}-Exercise");
+                        }
+                    }
+                }
+                else if (command == "Exercise")
+                {
+                    int index = initialLessons.IndexOf(lessonTitle);
+
+                    if (initialLessons.Contains(lessonTitle) && !initialLessons.Contains($"{lessonTitle}-Exercise"))
+                    {
+                        initialLessons.Insert(index + 1, $"{lessonTitle}-Exercise");
+                    }
+                    else if (!initialLessons.Contains(lessonTitle))
+                    {
+                        initialLessons.Add(lessonTitle);
+                        initialLessons.Add($"{lessonTitle}-Exercise");
+                    }
+                }
+
+                cmdArgs = Console.ReadLine().Split(':').ToArray();
+            }
+
+            for (int i = 0; i < initialLessons.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{initialLessons[i]}");
+            }
+        }
+    }
+}

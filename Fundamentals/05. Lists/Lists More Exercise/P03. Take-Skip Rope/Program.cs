f13825ac90286e88cb7e985cda61056d7b2619f6
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-07 12:07:09 +0300

    Uploading new files

---
+++ Fundamentals/05. Lists/Lists More Exercise/P03. Take-Skip Rope/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Take_Skip_Rope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> text = Console.ReadLine().ToList();
+            List<int> digits = new List<int>();
+
+            for (int i = 0; i < text.Count; i++)
+            {
+                if (char.IsNumber(text[i]))
+                {
+                    int currNum = int.Parse(text[i].ToString());
+                    digits.Add(currNum);
+                    text.Remove(text[i]);
+                    i--;
+                }
+            }
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+
+            for (int i = 0; i < digits.Count; i++)
+            {
+                if (i % 2 == 0) takeList.Add(digits[i]);
+                else skipList.Add(digits[i]);
+            }
+            
+            string result = String.Empty;
+            int index = 0;
+
+            for (int i = 0; i < skipList.Count; i++)
+            {
+                int skipCount = skipList[i];
+                int takeCount = takeList[i];
+                result += new string(text.Skip(index).Take(takeCount).ToArray());
+                index += skipCount + takeCount;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}

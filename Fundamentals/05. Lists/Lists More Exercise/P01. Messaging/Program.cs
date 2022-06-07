7afb9f814af1fb9b4f9d57c9495862db16da41df
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-07 10:19:43 +0300

    Uploading new files

---
+++ Fundamentals/05. Lists/Lists More Exercise/P01. Messaging/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<char> text = Console.ReadLine().ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                int index = 0;
+                int currNum = input[i];
+
+                while (currNum > 0)
+                {
+                    int currDigit = currNum % 10;
+                    index += currDigit;
+                    currNum /= 10;
+                }
+
+                if (index > text.Count)
+                {
+                    index -= text.Count;
+                }
+
+                Console.Write(text[index]);
+                text.RemoveAt(index);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fundamentals/05. Lists/Lists More Exercise/P02. Car Race/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Car_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            double leftTime = 0;
+            double rightTime = 0;
+
+            for (int i = 0; i < input.Count / 2; i++)
+            {
+                leftTime += input[i];
+                if (input[i] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+            }
+
+            for (int i = input.Count - 1; i > input.Count / 2; i--)
+            {
+                rightTime += input[i];
+                if (input[i] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+            }
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime}");
+            }
+        }
+    }
+}

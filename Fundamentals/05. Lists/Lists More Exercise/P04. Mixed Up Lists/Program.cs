e7acf3dcba25bd9d6247baef5722a3aa493733a9
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-08 14:14:21 +0300

    Uploading new files

---
+++ Fundamentals/05. Lists/Lists More Exercise/P04. Mixed Up Lists/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04._Mixed_Up_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> combinedList = new List<int>();
+
+            int smallerList = Math.Min(firstList.Count, secondList.Count);
+            secondList.Reverse();
+
+            for (int i = 0; i < smallerList; i++)
+            {
+                combinedList.Add(firstList[i]);
+                combinedList.Add(secondList[i]);
+            }
+
+            int constraitOne = 0;
+            int constraitTwo = 0;
+
+            if (firstList.Count > secondList.Count)
+            {
+                constraitOne = firstList[firstList.Count - 1];
+                constraitTwo = firstList[firstList.Count - 2];
+            }
+            else
+            {
+                constraitOne = secondList[secondList.Count-1];
+                constraitTwo = secondList[secondList.Count - 2];
+            }
+
+            int smallerCons = Math.Min(constraitTwo, constraitOne);
+            int biggerCons = Math.Max(constraitOne, constraitTwo);
+            List<int> listToPrint = new List<int>();
+
+            for (int i = 0; i < combinedList.Count; i++)
+            {
+                if (combinedList[i] > smallerCons && combinedList[i] < biggerCons)
+                {
+                    listToPrint.Add(combinedList[i]);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            listToPrint.Sort();
+
+            Console.WriteLine(string.Join(' ', listToPrint));
+        }
+    }
+}
---
+++ Fundamentals/05. Lists/Lists More Exercise/P05. Drum Set/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05._Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<double> drums = Console.ReadLine().Split().Select(double.Parse).ToList();
+            string hitPower = Console.ReadLine();
+
+            List<double> drumsInitial = new List<double>();
+            drumsInitial.AddRange(drums);
+
+            while (hitPower != "Hit it again, Gabsy!")
+            {
+                double power = double.Parse(hitPower);
+
+                for (int i = 0; i < drums.Count; i++)
+                {
+                    drums[i] -= power;
+
+                    if (drums[i] <= 0)
+                    {
+                        if (savings - (drumsInitial[i] * 3) >= 0)
+                        {
+                            savings -= drumsInitial[i] * 3;
+                            drums[i] = drumsInitial[i];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < drums.Count; i++)
+                {
+                    if (drums[i] <= 0)
+                    {
+                        drums.Remove(drums[i]);
+                        drumsInitial.Remove(drumsInitial[i]);
+                    }
+                }
+
+                hitPower = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', drums));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}

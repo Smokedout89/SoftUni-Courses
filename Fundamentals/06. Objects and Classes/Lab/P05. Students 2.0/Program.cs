1ebd014c00fdd6704f4568afb8e11babe8e59530
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-10 10:29:54 +0300

    Uploading new files

---
+++ Fundamentals/06. Objects and Classes/Lab/P05. Students 2.0/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P4._Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, string age, string town)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Town { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<Student> students = new List<Student>();
+
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split(' ');
+
+                string firstName = cmdArgs[0];
+                string lastName = cmdArgs[1];
+                string age = cmdArgs[2];
+                string town = cmdArgs[3];
+
+                if (IsStudentExisting(students, firstName,lastName))
+                {
+                    Student student = GetStudent(students, firstName, lastName);
+
+                    student.Age = age;
+                    student.Town = town;
+                }
+                else
+                {
+                    Student student = new Student(firstName, lastName, age, town);
+
+                    students.Add(student);
+                }
+                command = Console.ReadLine();
+            }
+
+            string townNeeded = Console.ReadLine();
+
+            List<Student> sortedStudents = students.FindAll(student => student.Town == townNeeded);
+
+            foreach (Student student in sortedStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+
+        private static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        private static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Fundamentals/06. Objects and Classes/Lab/P06. Store Boxes/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P6._Store_Boxes
+{
+    class Item
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceBox { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<Box> boxes = new List<Box>();
+            List<Item> items = new List<Item>();
+
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split(' ');
+
+                string serialNumber = cmdArgs[0];
+                string item = cmdArgs[1];
+                int itemQuantity = int.Parse(cmdArgs[2]);
+                decimal price = decimal.Parse(cmdArgs[3]);
+
+                Item newItem = new Item()
+                {
+                    Name = item,
+                    Price = price
+                };
+
+                items.Add(newItem);
+
+                Box newBox = new Box()
+                {
+                    SerialNumber = serialNumber,
+                    Item = newItem,
+                    ItemQuantity = itemQuantity,
+                    PriceBox = itemQuantity * newItem.Price
+                };
+
+                boxes.Add(newBox);
+
+                command = Console.ReadLine();   
+            }
+
+            List<Box> filteredBoxes = boxes.OrderByDescending(box => box.PriceBox).ToList();
+
+            foreach (Box box in filteredBoxes)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceBox:f2}");
+            }
+        }
+    }
+}

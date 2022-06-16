29b4e66caf463a4162be73c053eea744dd21bca6
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-16 09:43:32 +0300

    Uploading new files

---
+++ Fundamentals/06. Objects and Classes/Exercise and More Exercises/P11. Raw Data/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11._Raw_Data
+{
+    class Car
+    {
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType) 
+        {
+            this.Model = model;
+            this.CarsEngine = new Engine(engineSpeed, enginePower);
+            this.CarsCargo = new Cargo(cargoWeight, cargoType);
+        }
+        public Engine CarsEngine { get; set; }
+
+        public Cargo CarsCargo { get; set; }
+
+        public string Model { get; set; }
+    }
+
+    class Engine
+    {
+        public Engine(int speed, int power)
+        {
+            this.EngineSpeed = speed;
+            this.EnginePower = power;
+        }
+        public int EngineSpeed { get; set; }
+
+        public int EnginePower { get; set; }
+
+    }
+
+    class Cargo
+    {
+        public Cargo(int weight, string type)
+        {
+            this.CargoWeight = weight;
+            this.CargoType = type;
+        }
+        public int CargoWeight { get; set; }
+
+        public string CargoType { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string model = input[0];
+                int engineSpeed = int.Parse(input[1]);
+                int enginePower = int.Parse(input[2]);
+                int cargoWeight = int.Parse(input[3]);
+                string cargoType = input[4];
+
+                cars.Add(new Car(model, engineSpeed, enginePower, cargoWeight,cargoType));
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                foreach (Car car in cars.Where(c => c.CarsCargo.CargoType == "fragile" && c.CarsCargo.CargoWeight < 1000))
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+
+            if (command == "flamable")
+            {
+                foreach (Car car in cars.Where(c => c.CarsCargo.CargoType == "flamable" && c.CarsEngine.EnginePower > 250))
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/06. Objects and Classes/Exercise and More Exercises/P12. Shopping Spree/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P11._Shopping_Spree
+{
+    class Person
+    {
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+        public string Name { get; set; }
+
+        public decimal Money { get; set; }
+
+        public List<string> Bag { get; set; } = new List<string>();
+
+        public void BuyProduct(Product productToBuy)
+        {
+            if (this.Money >= productToBuy.Price)
+            {
+                this.Bag.Add(productToBuy.Name);
+                this.Money -= productToBuy.Price;
+                Console.WriteLine($"{this.Name} bought {productToBuy.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {productToBuy.Name}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            List<Person> people = new List<Person>();
+
+            string[] cmdPeople = Console.ReadLine().Split(';').ToArray();
+
+            foreach (var person in cmdPeople)
+            {
+                string name = person.Split('=')[0];
+                decimal money = decimal.Parse(person.Split('=')[1]);
+                people.Add(new Person(name, money));
+            }
+
+            string[] cmdProducts = Console.ReadLine().Split(';').ToArray();
+
+            foreach (var product in cmdProducts)
+            {
+                string name = product.Split('=')[0];
+                decimal price = decimal.Parse(product.Split('=')[1]);
+                products.Add(new Product(name, price));
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string personName = command.Split()[0];
+                string productToBuy = command.Split()[1];
+
+                people.Find(p => p.Name == personName).BuyProduct(products.Find(p => p.Name == productToBuy));
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var person in people)
+            {
+                if (person.Bag.Count > 0)
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.Bag)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+            }
+        }
+    }
+}

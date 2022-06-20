9bba64afdac571be3ee0fd0dadb10d9c3c8af573
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-20 18:19:17 +0300

    Uploading new files

---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P04. Word Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0)  
+                .ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P05. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Join(string.Empty, Console.ReadLine().Split());
+
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+
+            foreach (char letter in text)
+            {
+                if (countChars.ContainsKey(letter))
+                {
+                    countChars[letter]++;
+                }
+                else
+                {
+                    countChars.Add(letter, 1);
+                }
+            }
+
+            foreach (var character in countChars)
+            {
+                Console.WriteLine($"{character.Key} -> {character.Value}");
+            }
+        }
+    }
+}

f8693b91823c441042d2b91df2c00c6ac92e83c7
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-22 17:29:35 +0300

    Uploading new files

---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P09. SoftUni Parking/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P09._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> parkingLot = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                string command = cmdArgs[0];
+                string username = cmdArgs[1];
+
+                if (command == "register")
+                {
+                    string licensePlate = cmdArgs[2];
+
+                    if (parkingLot.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                        continue;
+                    }
+
+                    parkingLot.Add(username, licensePlate);
+                    Console.WriteLine($"{username} registered {licensePlate} successfully");
+                }
+
+                if (command == "unregister")
+                {
+                    if (!parkingLot.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                        continue;
+                    }
+
+                    parkingLot.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                }
+            }
+
+            foreach (var car in parkingLot)
+            {
+                Console.WriteLine($"{car.Key} => {car.Value}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P10. Courses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P10._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = command.Split(" : ");
+                string currCours = cmdArgs[0];
+                string currStudent = cmdArgs[1];
+
+                if (!courses.ContainsKey(currCours))
+                {
+                    courses.Add(currCours, new List<string>());
+                }
+
+                courses[currCours].Add(currStudent);
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                
+                foreach (var studentName in course.Value)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+            }
+        }
+    }
+}

e97bc6c0e971082e7219202b16611e6cdbebf141
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-23 10:31:12 +0300

    Uploading new files

---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P11. Student Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentGrades.ContainsKey(name))
+                {
+                    studentGrades.Add(name, new List<double>());
+                }
+
+                studentGrades[name].Add(grade);
+            }
+
+            foreach (var student in studentGrades.Where(student => student.Value.Average() >= 4.50))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/07. Associative Arrays/Lab and Exercise/P12. Company Users/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P12._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split(" -> ");
+                string company = cmdArgs[0];
+                string userID = cmdArgs[1];
+
+                if (!companyUsers.ContainsKey(company))
+                {
+                    companyUsers.Add(company, new List<string>());
+                }
+
+                if (companyUsers[company].Contains(userID))
+                {
+                    continue;
+                }
+
+                companyUsers[company].Add(userID);
+            }
+
+            foreach (var kvp in companyUsers)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var user in kvp.Value)
+                {
+                    Console.WriteLine($"-- {user}");
+                }
+            }
+        }
+    }
+}

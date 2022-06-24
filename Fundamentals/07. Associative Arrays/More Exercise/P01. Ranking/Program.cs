ea2be3081feb3e5c8d6eac40164748b989fc8e5d
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-24 10:46:02 +0300

    Uploading new files

---
+++ Fundamentals/07. Associative Arrays/More Exercise/P01. Ranking/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> passwords = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> userPoints = new Dictionary<string, Dictionary<string, int>>();
+
+            string passwordsInput = Console.ReadLine();
+
+            while (passwordsInput != "end of contests")
+            {
+                string[] input = passwordsInput.Split(':');
+                string contest = input[0];
+                string password = input[1];
+
+                passwords.Add(contest,password);
+
+                passwordsInput = Console.ReadLine();
+            }
+
+            string pointsInput = Console.ReadLine();
+
+            while (pointsInput != "end of submissions")
+            {
+                string[] cmdArgs = pointsInput.Split("=>");
+                string contest = cmdArgs[0];
+                string password = cmdArgs[1];
+                string name = cmdArgs[2];
+                int points = int.Parse(cmdArgs[3]);
+
+                if (passwords.ContainsKey(contest) && passwords[contest] == password)
+                {
+                    if (!userPoints.ContainsKey(name))
+                    {
+                        userPoints.Add(name, new Dictionary<string, int>());
+                    }
+
+                    if (!userPoints[name].ContainsKey(contest))
+                    {
+                        userPoints[name].Add(contest, 0);
+                    }
+
+                    if (userPoints[name][contest] < points)
+                    {
+                        userPoints[name][contest] = points;
+                    }
+                }
+
+                pointsInput = Console.ReadLine();
+            }
+
+            foreach (var user in userPoints.OrderByDescending(kvp => kvp.Value.Values.Sum()))
+            {
+                Console.WriteLine($"Best candidate is {user.Key} with total {user.Value.Values.Sum()} points.");
+                break;
+            }
+
+            Console.WriteLine("Ranking: ");
+
+            foreach (var student in userPoints.OrderBy(n => n.Key))
+            {
+                Console.WriteLine($"{student.Key}");
+
+                foreach (var kvp in student.Value.OrderByDescending(kvp => kvp.Value))
+                {
+                    Console.WriteLine($"#  {kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/07. Associative Arrays/More Exercise/P02. Judge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> contestInfo = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, Dictionary<string, int>> individualStanding = new Dictionary<string, Dictionary<string, int>>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "no more time")
+            {
+                string[] cmdArgs = command.Split(" -> ");
+                string student = cmdArgs[0];
+                string contest = cmdArgs[1];
+                int points = int.Parse(cmdArgs[2]);
+
+                if (!contestInfo.ContainsKey(contest))
+                {
+                    contestInfo.Add(contest, new Dictionary<string, int>());
+                }
+
+                if (!contestInfo[contest].ContainsKey(student))
+                {
+                    contestInfo[contest].Add(student, 0);
+                }
+
+                if (contestInfo[contest][student] < points)
+                {
+                    contestInfo[contest][student] = points;
+                }
+
+                if (!individualStanding.ContainsKey(student))
+                {
+                    individualStanding.Add(student, new Dictionary<string, int>());
+                }
+
+                if (!individualStanding[student].ContainsKey(contest))
+                {
+                    individualStanding[student].Add(contest, 0);
+                }
+
+                if (individualStanding[student][contest] < points)
+                {
+                    individualStanding[student][contest] = points;
+                }
+
+            }
+
+            foreach (var contest in contestInfo)
+            {
+                Console.WriteLine($"{contest.Key}: {contest.Value.Count} participants");
+
+                int index = 1;
+
+                foreach (var student in contest.Value.OrderByDescending(points => points.Value).ThenBy(name => name.Key))
+                {
+                    Console.WriteLine($"{index}. {student.Key} <::> {student.Value}");
+                    index++;
+                }
+            }
+
+            Console.WriteLine("Individual standings:");
+
+            int individualIndex = 1;
+
+            foreach (var student in individualStanding.OrderByDescending(totalpts => totalpts.Value.Values.Sum()).ThenBy(name => name.Key))
+            {
+                Console.WriteLine($"{individualIndex}. {student.Key} -> {student.Value.Values.Sum()}");
+                individualIndex++;
+            }
+        }
+    }
+}

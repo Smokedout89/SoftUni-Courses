8143c88ba69e2417f499f9c07c56a2368e161cc7
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-06-27 11:04:40 +0300

    Uploading new files

---
+++ Fundamentals/07. Associative Arrays/More Exercise/P03. MOBA Challenger/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03._MOBA_Challenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> playersDetails = new Dictionary<string, Dictionary<string, int>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Season end")
+            {
+                if (command.Contains(" -> "))
+                {
+                    string[] cmdArgs = command.Split(" -> ");
+                    string playerName = cmdArgs[0];
+                    string position = cmdArgs[1];
+                    int skill = int.Parse(cmdArgs[2]);
+
+                    if (!playersDetails.ContainsKey(playerName))
+                    {
+                        playersDetails.Add(playerName, new Dictionary<string, int>());
+                    }
+
+                    if (!playersDetails[playerName].ContainsKey(position))
+                    {
+                        playersDetails[playerName].Add(position, 0);
+                    }
+
+                    if (playersDetails[playerName][position] < skill)
+                    {
+                        playersDetails[playerName][position] = skill;
+                    }
+
+                }
+                else if (command.Contains(" vs "))
+                {
+                    string[] cmdArgs = command.Split(" vs ");
+                    string playerOne = cmdArgs[0];
+                    string playerTwo = cmdArgs[1];
+
+                    if (playersDetails.ContainsKey(playerOne) && playersDetails.ContainsKey(playerTwo))
+                    {
+                        foreach (var position in playersDetails[playerOne].Keys)
+                        {
+                            if (playersDetails[playerTwo].ContainsKey(position))
+                            {
+                                if (playersDetails[playerOne].Values.Sum() > playersDetails[playerTwo].Values.Sum())
+                                {
+                                    playersDetails.Remove(playerTwo);
+                                }
+                                else if (playersDetails[playerTwo].Values.Sum() > playersDetails[playerOne].Values.Sum())
+                                {
+                                    playersDetails.Remove(playerOne);
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var player in playersDetails.OrderByDescending(skill => skill.Value.Values.Sum()).ThenBy(name => name.Key))
+            {
+
+                Console.WriteLine($"{player.Key}: {player.Value.Values.Sum()} skill");
+
+                foreach (var kvp in player.Value.OrderByDescending(skill => skill.Value).ThenBy(pos => pos.Key))
+                {
+                    Console.WriteLine($"- {kvp.Key} <::> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/07. Associative Arrays/More Exercise/P04. Snowwhite/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04._Snowwhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dwarfs = new Dictionary<string, int>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Once upon a time")
+            {
+                string[] inputArr = inputLine.Split(" <:> ");
+                string dwarfName = inputArr[0];
+                string hatColour = inputArr[1];
+                int physics = int.Parse(inputArr[2]);
+
+                string key = $"({hatColour}) {dwarfName}";
+
+                if (!dwarfs.ContainsKey(key))
+                {
+                    dwarfs.Add(key, 0);
+                }
+
+                if (dwarfs[key] < physics)
+                {
+                    dwarfs[key] = physics;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var dwarf in dwarfs.OrderByDescending(kvp => kvp.Value)
+                         .ThenByDescending(c => dwarfs.Where(kvp => kvp.Key.Split(")")[0] == c.Key.Split(")")[0]).Count()))
+            {
+                Console.WriteLine($"{dwarf.Key} <-> {dwarf.Value}");
+            }
+        }
+    }
+}

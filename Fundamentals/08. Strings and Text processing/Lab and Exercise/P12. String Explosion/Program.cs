aa00840be2a1c8f6326cc354715123e26867b938
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-07-01 12:13:30 +0300

    Uploading new files

---
+++ Fundamentals/08. Strings and Text processing/Lab and Exercise/P12. String Explosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P12._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += input[i + 1] - '0';
+                }
+                else if (strength > 0)
+                {
+                    input = input.Remove(i, 1);
+                    strength--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Fundamentals/08. Strings and Text processing/Lab and Exercise/P13. Letters Change Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace P13._Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            decimal sum = 0;
+
+            foreach (string word in words)
+            {
+                int firstNumPos = GetAlphabeticalPosition(word[0]);
+                int lastNumPos = GetAlphabeticalPosition(word[word.Length - 1]);
+                decimal number = GetNumberFromString(word);
+
+                if (char.IsUpper(word[0]))
+                {
+                    number /= firstNumPos;
+                }
+                else if (char.IsLower(word[0]))
+                {
+                    number *= firstNumPos;
+                }
+
+                if (char.IsUpper(word[word.Length - 1]))
+                {
+                    number -= lastNumPos;
+                }
+                else if (char.IsLower(word[word.Length - 1]))
+                {
+                    number += lastNumPos;
+                }
+
+                sum += number;
+
+            }
+
+                Console.WriteLine($"{sum:f2}");
+        }
+
+        static int GetNumberFromString(string word)
+        {
+            int result;
+            word = word.Substring(1, word.Length - 2);
+
+            return result = int.Parse(word);
+        }
+        static int GetAlphabeticalPosition(char ch)
+        {
+            return char.ToLower(ch) - 96;
+        }
+    }
+}

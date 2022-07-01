9178e08d77fc8f585f88ba574f4f66c4ff5ad743
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-07-01 10:36:40 +0300

    Uploading new files

---
+++ Fundamentals/08. Strings and Text processing/Lab and Exercise/P10. Multiply Big Number/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace P10._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNum = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+
+            if (multiplier == 0)
+            {
+                //result = new string('0', inputNum.Length);
+                Console.WriteLine(0);
+            }
+
+            int remainder = 0;
+
+            for (int i = inputNum.Length - 1; i >= 0; i--)
+            {
+                int currNum = inputNum[i] - '0';
+
+                int multiplied = multiplier * currNum;
+
+                if (remainder != 0)
+                {
+                    multiplied += remainder;
+                    remainder = 0;
+                }
+
+                int current = multiplied;
+
+                if (multiplied > 9)
+                {
+                    current = multiplied % 10;
+                    remainder = multiplied / 10;
+                }
+
+                result += current.ToString();
+            }
+
+            if (remainder != 0)
+            {
+                result += remainder;
+            }
+
+            char[] reverseIt = result.ToCharArray();
+            Array.Reverse(reverseIt);
+
+            if (multiplier != 0)
+            {
+                Console.WriteLine(reverseIt);
+            }
+        }
+    }
+}
---
+++ Fundamentals/08. Strings and Text processing/Lab and Exercise/P11. Replace Repeating Chars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P11._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+          
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    result += input[i];
+                }
+                else if (input[i] != input[i + 1])
+                {
+                    result += input[i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

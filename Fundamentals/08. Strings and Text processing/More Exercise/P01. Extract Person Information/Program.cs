2cfb52abd9e44bed97fc8f51cb67c9d60e110aea
P.Velikov <95408917+Smokedout89@users.noreply.github.com>
2022-07-04 10:21:59 +0300

    Uploading new files

---
+++ Fundamentals/08. Strings and Text processing/More Exercise/P01. Extract Person Information/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                int firstIndex = input.IndexOf('@');
+                int secondIndex = input.IndexOf('|');
+                int firstLength = secondIndex - firstIndex - 1;
+                int thirdIndex = input.IndexOf('#');
+                int forthIndex = input.IndexOf('*');
+                int secondLenght = forthIndex - thirdIndex - 1;
+
+                string name = input.Substring(firstIndex + 1, firstLength);
+                string age = input.Substring(thirdIndex + 1, secondLenght);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ Fundamentals/08. Strings and Text processing/More Exercise/P02. Ascii Sumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCh = char.Parse(Console.ReadLine());
+            char secondCh = char.Parse(Console.ReadLine());
+            string randomString = Console.ReadLine();
+
+            int sum = 0;
+
+            foreach (char ch in randomString)
+            {
+                if (ch > firstCh && ch < secondCh)
+                {
+                    sum += ch;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

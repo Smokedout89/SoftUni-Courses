39474939cc9e524ac8004fda2b8232ce71f504c0
Smokedout89 <smokedout8989@gmail.com>
2022-07-11 10:34:29 +0300

    Uploading new files

---
+++ Fundamentals/09. RegEx/Lab + Exercise + More/P10. Winning Ticket - More Exercise/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+ 
+namespace WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ticket = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string pattern = @"(\@{6,}|\${6,}|\^{6,}|\#{6,})";
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < ticket.Length; i++)
+            {
+                if (ticket[i].Length == 20)
+                {
+                    Match firstHalf = regex.Match(ticket[i].Substring(0, 10));
+                    Match secondHalf = regex.Match(ticket[i].Substring(10));
+                    int minLength = Math.Min(firstHalf.Length, secondHalf.Length);
+
+                    if (firstHalf.Success && secondHalf.Success)
+                    {
+                        string winFirstHalf = firstHalf.Value.Substring(0, minLength);
+                        string winSecondHalf = secondHalf.Value.Substring(0, minLength);
+
+                        if (winFirstHalf.Equals(winSecondHalf))
+                        {
+                            if (winFirstHalf.Length == 10)
+                            {
+                                Console.WriteLine($"ticket \"{ticket[i]}\" - {minLength}{winFirstHalf.Substring(0, 1)} Jackpot!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"ticket \"{ticket[i]}\" - {minLength}{winFirstHalf.Substring(0, 1)}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{ticket[i]}\" - no match");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{ticket[i]}\" - no match");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/09. RegEx/Lab + Exercise + More/P11. Rage Quit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P11._Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

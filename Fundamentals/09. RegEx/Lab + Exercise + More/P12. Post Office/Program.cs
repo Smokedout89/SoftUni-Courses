c206018b3178917d9b4b39123c80dcb5cd1d2ad6
Smokedout89 <smokedout8989@gmail.com>
2022-07-11 11:25:38 +0300

    Uploading new files

---
+++ Fundamentals/09. RegEx/Lab + Exercise + More/P12. Post Office/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P12._Post_Office
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

6280f1fc5de5f4b548882dd52b614a01ce37fecb
Smokedout89 <smokedout8989@gmail.com>
2022-07-18 11:07:08 +0300

    Uploading new files

---
+++ Fundamentals/Mid Exam/Exam/P01 - Cooking Masterclass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P01___Cooking_Masterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPrice = double.Parse(Console.ReadLine());
+            double eggPrice = double.Parse(Console.ReadLine());
+            double apronPrice = double.Parse(Console.ReadLine());
+
+            double studentsForApron = Math.Ceiling(students + students * 0.2);
+            int freeFlour = 0;
+
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    freeFlour++;
+                }
+            }
+
+            double totalSpent = (apronPrice * studentsForApron) + (eggPrice * 10 * students) +
+                                     (flourPrice * (students - freeFlour));
+
+            if (budget >= totalSpent)
+            {
+                Console.WriteLine($"Items purchased for {totalSpent:f2}$.");
+            }
+            else
+            {
+                double moneyNeeded = totalSpent - budget;
+                Console.WriteLine($"{moneyNeeded:f2}$ more needed.");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Mid Exam/Exam/P03 - Chat Logger/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03___Chat_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> result = new List<string>();
+
+            while (input != "end")
+            {
+                string[] token = input.Split();
+                string command = token[0];
+
+                if (command == "Chat")
+                {
+                    string msg = token[1];
+                    result.Add(msg);
+                }
+                else if (command == "Delete")
+                {
+                    string msg = token[1];
+                    if (result.Contains(msg))
+                    {
+                        result.Remove(msg);
+                    }
+                }
+                else if (command == "Edit")
+                {
+                    string msg = token[1];
+                    string newMsg = token[2];
+                    if (result.Contains(msg))
+                    {
+                        int index = result.IndexOf(msg);
+                        result.Remove(msg);
+                        result.Insert(index, newMsg);
+                    }
+                }
+                else if (command == "Pin")
+                {
+                    string msg = token[1];
+                    if (result.Contains(msg))
+                    {
+                        string saveIt = msg;
+                        result.Remove(msg);
+                        result.Add(saveIt);
+                    }
+                }
+                else if (command == "Spam")
+                {
+                    for (int i = 1; i < token.Length; i++)
+                    {
+                        string currWord = token[i];
+                        result.Add(currWord);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var elemnt in result)
+            {
+                Console.WriteLine(elemnt);
+            }
+        }
+    }
+}

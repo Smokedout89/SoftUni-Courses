b512a3bdf6a254324e43fdb85fe87054b0bcbff7
Smokedout89 <smokedout8989@gmail.com>
2022-07-22 10:25:16 +0300

    Uploading new files

---
+++ Fundamentals/Final Exam/Preparations/P10. The Imitation Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _10._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMsg = Console.ReadLine();
+
+            string inputCmd = Console.ReadLine();
+
+            while (inputCmd != "Decode")
+            {
+                string[] cmdArgs = inputCmd.Split('|');
+                string command = cmdArgs[0];
+
+                if (command == "Move")
+                {
+                    int numToMove = int.Parse(cmdArgs[1]);
+                    string modified = encryptedMsg.Substring(0, numToMove);
+                    encryptedMsg = encryptedMsg.Remove(0, numToMove);
+                    encryptedMsg += modified;
+                }
+                else if (command == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string value = cmdArgs[2];
+
+                    encryptedMsg = encryptedMsg.Insert(index, value);
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = cmdArgs[1];
+                    string replacement = cmdArgs[2];
+
+                    encryptedMsg = encryptedMsg.Replace(substring, replacement);
+                }
+
+                inputCmd = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMsg}");
+        }
+    }
+}
---
+++ Fundamentals/Final Exam/Preparations/P11. Ad Astra/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _11._Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"(\||#)(?<name>[A-Za-z\s?]+)(\1)(?<date>\d{2}\/\d{2}\/\d{2})(\1)(?<calories>\d{1,5})(\1)");
+
+            MatchCollection matches = regex.Matches(input);
+            int totalCal = 0;
+
+            foreach (Match match in matches)
+            {
+                int calories = int.Parse(match.Groups["calories"].Value);
+                totalCal += calories;
+            }
+
+            int days = totalCal / 2000;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match match in matches)
+            {
+                string name = match.Groups["name"].Value;
+                string date = match.Groups["date"].Value;
+                int calories = int.Parse(match.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {name}, Best before: {date}, Nutrition: {calories}");
+            }
+        }
+    }
+}

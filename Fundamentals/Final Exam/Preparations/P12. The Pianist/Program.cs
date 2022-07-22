e5c63c0b4d82441841845909ad219891b1168c50
Smokedout89 <smokedout8989@gmail.com>
2022-07-22 12:34:34 +0300

    Uploading new files

---
+++ Fundamentals/Final Exam/Preparations/P12. The Pianist/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12._The_Pianist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> pieceComposer = new Dictionary<string, string>();
+            Dictionary<string, string> pieceKey = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] argInfo = Console.ReadLine().Split('|');
+                string piece = argInfo[0];
+                string composer = argInfo[1];
+                string key = argInfo[2];
+
+                pieceComposer.Add(piece, composer);
+                pieceKey.Add(piece, key);
+            }
+
+            string cmd = Console.ReadLine();
+
+            while (cmd != "Stop")
+            {
+                string[] cmdArgs = cmd.Split('|');
+                string command = cmdArgs[0];
+                string piece = cmdArgs[1];
+
+                if (command == "Add")
+                {
+                    string composer = cmdArgs[2];
+                    string key = cmdArgs[3];
+
+                    if (!pieceComposer.ContainsKey(piece))
+                    {
+                        pieceComposer.Add(piece, composer);
+                        pieceKey.Add(piece, key);
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    if (pieceComposer.ContainsKey(piece))
+                    {
+                        pieceComposer.Remove(piece);
+                        pieceKey.Remove(piece);
+                        Console.WriteLine($"Successfully removed {piece}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+                else if (command == "ChangeKey")
+                {
+                    string newKey = cmdArgs[2];
+
+                    if (pieceKey.ContainsKey(piece))
+                    {
+                        pieceKey[piece] = newKey;
+                        Console.WriteLine($"Changed the key of {piece} to {newKey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+
+                cmd = Console.ReadLine();
+            }
+
+            foreach (var composer in pieceComposer)
+            {
+                string piece = composer.Key;
+                string key = pieceKey[piece];
+                
+                Console.WriteLine($"{composer.Key} -> Composer: {composer.Value}, Key: {key}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Final Exam/Preparations/P13. Need for Speed III/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13._Need_for_Speed_III
+{
+    class Car
+    {
+        public Car(int mileage, int fuel)
+        {
+            this.Mileage = mileage;
+            this.Fuel = fuel;
+        }
+
+        public int Mileage { get; set; }
+
+        public int Fuel { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] details = Console.ReadLine().Split('|');
+                string carModel = details[0];
+                int mileage = int.Parse(details[1]);
+                int fuel = int.Parse(details[2]);
+
+                cars.Add(carModel, new Car(mileage, fuel));
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                string[] cmdArgs = input.Split(" : ");
+                string command = cmdArgs[0];
+                string car = cmdArgs[1];
+
+                if (command == "Drive")
+                {
+                    int distance = int.Parse(cmdArgs[2]);
+                    int fuel = int.Parse(cmdArgs[3]);
+                    int carMaxMileage = 100000;
+
+                    if (cars[car].Fuel >= fuel)
+                    {
+                        cars[car].Fuel -= fuel;
+                        cars[car].Mileage += distance;
+                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+
+                    if (cars[car].Mileage > carMaxMileage)
+                    {
+                        Console.WriteLine($"Time to sell the {car}!");
+                        cars.Remove(car);
+                    }
+
+                }
+                else if (command == "Refuel")
+                {
+                    int fuel = int.Parse(cmdArgs[2]);
+                    int carMaxFuel = 75;
+                    cars[car].Fuel += fuel;
+
+                    if (cars[car].Fuel > carMaxFuel)
+                    {
+                        fuel = Math.Abs(cars[car].Fuel - carMaxFuel - fuel);
+                        cars[car].Fuel = carMaxFuel;
+                    }
+
+                    Console.WriteLine($"{car} refueled with {fuel} liters");
+                }
+                else if (command == "Revert")
+                {
+                    int kmToDecrease = int.Parse(cmdArgs[2]);
+                    int minMileage = 10000;
+
+                    cars[car].Mileage -= kmToDecrease;
+
+                    if (cars[car].Mileage <= minMileage)
+                    {
+                        cars[car].Mileage = minMileage;
+                        break;
+                    }
+
+                    Console.WriteLine($"{car} mileage decreased by {kmToDecrease} kilometers");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Key} -> Mileage: {car.Value.Mileage} kms, Fuel in the tank: {car.Value.Fuel} lt.");
+            }
+        }
+    }
+}

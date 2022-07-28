49a36f0b60d20f95da5c4dbe9623d6c066cb24db
Smokedout89 <smokedout8989@gmail.com>
2022-07-28 12:28:08 +0300

    Uploading new files

---
+++ Advanced/01. Stacks and Queues/Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //Queue<int> queue = new Queue<int>(numbers.Where(n => n % 2 == 0).ToArray());
+            //Console.WriteLine(string.Join(", ", queue));
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
---
+++ Advanced/01. Stacks and Queues/Lab/06. Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> names = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (string name in names)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    names.Clear();
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}

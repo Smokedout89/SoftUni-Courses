9208d2acadd3a1c4758e7de94e6c315dcfa86278
Smokedout89 <smokedout8989@gmail.com>
2022-07-29 10:38:42 +0300

    Uploading new files

---
+++ Advanced/01. Stacks and Queues/Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int number = int.Parse(Console.ReadLine());
+            Queue<string> names = new Queue<string>(input);
+
+            while (names.Count > 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    names.Enqueue(names.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {names.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {names.Dequeue()}");
+        }
+    }
+}
---
+++ Advanced/01. Stacks and Queues/Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsCanPass = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int passedCars = 0;
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < carsCanPass; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Peek()} passed!");
+                            cars.Dequeue();
+                            passedCars++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}

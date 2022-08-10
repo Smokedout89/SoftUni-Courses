ab03b8e723125a0e15f28e149108227853f70c1a
Smokedout89 <smokedout8989@gmail.com>
2022-08-10 11:37:07 +0300

    Uploading new files

---
+++ Advanced/02. Multidimensional Arrays/Lab/06. Jagged-Array Modification/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jagged[i] = ReadArrayFromConsole();
+            }
+
+            string command = Console.ReadLine().ToUpper();
+
+            while (command != "END")
+            {
+                string[] tokens = command.Split(' ');
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                int value = int.Parse(tokens[3]);
+
+                if (row < 0 || row >= n || col < 0 || col >= jagged[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    command = Console.ReadLine().ToUpper();
+                    continue;
+                }
+
+                switch (tokens[0])  
+                {
+                    case "ADD":
+                        jagged[row][col] += value;
+                        break;
+                    case "SUBTRACT":
+                        jagged[row][col] -= value;
+                        break;
+                }
+
+                command = Console.ReadLine().ToUpper();
+            }
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine(string.Join(' ', jagged[i]));
+            //}
+
+            foreach (int[] row in jagged)
+            {
+                Console.WriteLine(string.Join(' ', row));
+            }
+        }
+
+        static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Advanced/02. Multidimensional Arrays/Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[height][];
+
+            for (int i = 0; i < height; i++)
+            {
+                long[] row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    row[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+                }
+
+                triangle[i] = row;
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine(string.Join(' ', triangle[i]));
+            }
+        }
+    }
+}

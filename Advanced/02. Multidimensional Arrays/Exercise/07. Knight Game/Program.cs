ad53f133a38a79e9edafc2b26c496c0b38ea3752
Smokedout89 <smokedout8989@gmail.com>
2022-08-15 12:25:46 +0300

    Uploading new files

---
+++ Advanced/02. Multidimensional Arrays/Exercise/07. Knight Game/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace _07._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] elements = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            int knightsToRemove = 0;
+
+            while (true)
+            {
+                int maxAttacked = 0;
+                int knighToRemoveRow = 0;
+                int knightToRemoveCol = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col] == '0')
+                        {
+                            continue;
+                        }
+
+                        int currentAttacks = 0;
+                        // Up left and right
+                        if (IsInRange(matrix, row - 2, col - 1) && matrix[row - 2, col - 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        if (IsInRange(matrix, row - 2, col + 1) && matrix[row - 2, col + 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        // Down right and left
+                        if (IsInRange(matrix, row + 2, col + 1) && matrix[row + 2, col + 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        if (IsInRange(matrix, row + 2, col - 1) && matrix[row + 2, col - 1] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        // Left up and down
+                        if (IsInRange(matrix, row - 1, col - 2) && matrix[row - 1, col - 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        if (IsInRange(matrix, row + 1, col - 2) && matrix[row + 1, col - 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        // Right up and down
+                        if (IsInRange(matrix, row - 1, col + 2) && matrix[row - 1, col + 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+                        if (IsInRange(matrix, row + 1, col + 2) && matrix[row + 1, col + 2] == 'K')
+                        {
+                            currentAttacks++;
+                        }
+
+                        if (currentAttacks > maxAttacked)
+                        {
+                            maxAttacked = currentAttacks;
+                            knighToRemoveRow = row;
+                            knightToRemoveCol = col;
+                        }
+                    }
+                }
+
+                if (maxAttacked > 0)
+                {
+                    knightsToRemove++;
+                    matrix[knighToRemoveRow, knightToRemoveCol] = '0';
+                }
+                else
+                {
+                    Console.WriteLine(knightsToRemove);
+                    break;
+                }
+            }
+
+        }
+
+        private static bool IsInRange(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}
---
+++ Advanced/02. Multidimensional Arrays/Exercise/08. Bombs/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = elements[col];
+                }
+            }
+
+            int[] bombs = Console.ReadLine()
+                .Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < bombs.Length; i+=2)
+            {
+                int row = bombs[i];
+                int col = bombs[i + 1];
+
+                int bombPower = matrix[row,col];
+
+                if (bombPower <= 0)
+                {
+                    continue;
+                }
+                // Upper 3 
+                if (IsInMatrix(matrix,row - 1,col -1) && matrix[row - 1, col - 1] > 0)
+                {
+                    matrix[row - 1, col - 1] -= bombPower;
+                }
+                if (IsInMatrix(matrix, row - 1, col) && matrix[row - 1, col] > 0)
+                {
+                    matrix[row - 1, col] -= bombPower;
+                }
+                if (IsInMatrix(matrix, row - 1, col + 1) && matrix[row - 1, col + 1] > 0)
+                {
+                    matrix[row - 1, col + 1] -= bombPower;
+                }
+                // Same row left and right
+                if (IsInMatrix(matrix, row, col - 1) && matrix[row, col - 1] > 0)
+                {
+                    matrix[row, col - 1] -= bombPower;
+                }
+                if (IsInMatrix(matrix, row, col + 1) && matrix[row, col + 1] > 0)
+                {
+                    matrix[row, col + 1] -= bombPower;
+                }
+                // Lower 3 
+                if (IsInMatrix(matrix, row + 1, col - 1) && matrix[row + 1, col - 1] > 0)
+                {
+                    matrix[row + 1, col - 1] -= bombPower;
+                }
+                if (IsInMatrix(matrix, row + 1, col) && matrix[row + 1, col] > 0)
+                {
+                    matrix[row + 1, col] -= bombPower;
+                }
+                if (IsInMatrix(matrix, row + 1, col + 1) && matrix[row + 1, col + 1] > 0)
+                {
+                    matrix[row + 1, col + 1] -= bombPower;
+                }
+
+                matrix[row, col] = 0;
+            }
+
+            int aliveCells = 0;
+            int sum = 0;
+
+            foreach (var num in matrix)
+            {
+                if (num > 0)
+                {
+                    aliveCells++;
+                    sum += num;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsInMatrix(int[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}

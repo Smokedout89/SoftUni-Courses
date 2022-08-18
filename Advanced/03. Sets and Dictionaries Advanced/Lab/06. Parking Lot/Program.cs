66423417c430edf08cac006701f500c3bc5074c1
Smokedout89 <smokedout8989@gmail.com>
2022-08-18 11:08:18 +0300

    Uploading new files

---
+++ Advanced/03. Sets and Dictionaries Advanced/Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> parking = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] info = Regex.Split(input, ", ");
+
+                if (info[0] == "IN")
+                {
+                    parking.Add(info[1]);
+                }
+                else
+                {
+                    parking.Remove(info[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count > 0)
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine($"{car}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ Advanced/03. Sets and Dictionaries Advanced/Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regular = new HashSet<string>();
+            HashSet<string> VIP = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (char.IsDigit(input.First()))
+                {
+                    VIP.Add(input);
+                }
+                else
+                {
+                    regular.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (regular.Contains(input))
+                {
+                    regular.Remove(input);
+                }
+
+                if (VIP.Contains(input))
+                {
+                    VIP.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int guestsNotAttended = regular.Count + VIP.Count;
+
+            Console.WriteLine(guestsNotAttended);
+
+            foreach (var guest in VIP)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regular)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}

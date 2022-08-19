38f08ec14732e86acb5c4f44a1d5725edab5141c
Smokedout89 <smokedout8989@gmail.com>
2022-08-19 13:08:33 +0300

    Uploading new files

---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenghts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> one = new HashSet<int>();
+            HashSet<int> two = new HashSet<int>();
+
+            for (int i = 0; i < lenghts[0]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                one.Add(num);
+            }
+
+            for (int i = 0; i < lenghts[1]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                two.Add(num);
+            }
+
+            one.IntersectWith(two);
+
+            Console.WriteLine(string.Join(' ', one));
+        }
+    }
+}
---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> table = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    table.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', table.OrderBy(e => e)));
+        }
+    }
+}

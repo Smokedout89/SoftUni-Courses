be0fa05d6abe5d4e689db43a02f43ead36baceea
Smokedout89 <smokedout8989@gmail.com>
2022-08-23 10:51:12 +0300

    Uploading new files

---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/07. The V-Logger/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._The_V_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Dictionary<string, int[]> vloggersInfo = new Dictionary<string, int[]>();
+            Dictionary<string, List<string>> followers = new Dictionary<string, List<string>>();
+
+            while ((command = Console.ReadLine()) != "Statistics")
+            {
+                string[] cmdArgs = command.Split();
+                string action = cmdArgs[1];
+
+                if (action == "joined")
+                {
+                    string vlogger = cmdArgs[0];
+
+                    if (!vloggersInfo.ContainsKey(vlogger))
+                    {
+                        vloggersInfo.Add(vlogger, new int[2]);
+                        followers.Add(vlogger, new List<string>());
+                    }
+                }
+
+                if (action == "followed")
+                {
+                    string vlogger = cmdArgs[0];
+                    string followedVlogger = cmdArgs[2];
+
+                    if (!vloggersInfo.ContainsKey(vlogger) || !vloggersInfo.ContainsKey(followedVlogger))
+                    {
+                        continue;
+                    }
+
+                    if (vlogger == followedVlogger)
+                    {
+                        continue;
+                    }
+
+                    if (followers[followedVlogger].Contains(vlogger))
+                    {
+                        continue;
+                    }
+
+                    if (!followers[followedVlogger].Contains(vlogger))
+                    {
+                        followers[followedVlogger].Add(vlogger);
+                        vloggersInfo[vlogger][0]++;
+                        vloggersInfo[followedVlogger][1]++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggersInfo.Keys.Count} vloggers in its logs.");
+
+            int vloggerIndex = 1;
+
+            foreach (var vlogger in vloggersInfo.OrderByDescending(x => x.Value[1]).ThenBy(x => x.Value[0]))
+            {
+                Console.WriteLine($"{vloggerIndex}. {vlogger.Key} : {vlogger.Value[1]} followers, {vlogger.Value[0]} following");
+
+                if (vloggerIndex == 1)
+                {
+                    foreach (var follower in followers[vlogger.Key].OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {string.Join("\n", follower)}");
+                    }
+                }
+
+                vloggerIndex++;
+            }
+        }
+    }
+}
---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/08. Ranking/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string examsInfo;
+            Dictionary<string, string> examPass = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> studentExams = new Dictionary<string, Dictionary<string, int>>();
+
+            while ((examsInfo = Console.ReadLine()) != "end of contests")
+            {
+                string[] examDetails = examsInfo.Split(':');
+                examPass.Add(examDetails[0], examDetails[1]);
+            }
+
+            string examResults;
+
+            while ((examResults = Console.ReadLine()) != "end of submissions")
+            {
+                string[] cmdArgs = examResults.Split("=>");
+                string examName = cmdArgs[0];
+                string examPassword = cmdArgs[1];
+                string student = cmdArgs[2];
+                int points = int.Parse(cmdArgs[3]);
+
+                if (examPass.ContainsKey(examName) && examPass[examName] == examPassword)
+                {
+                    if (!studentExams.ContainsKey(student))
+                    {
+                        studentExams.Add(student, new Dictionary<string, int>());
+                    }
+
+                    if (studentExams[student].ContainsKey(examName))
+                    {
+                        if (studentExams[student][examName] < points)
+                        {
+                            studentExams[student][examName] = points;
+                            continue;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    studentExams[student].Add(examName, points);
+                }
+            }
+
+            string bestStudent = string.Empty;
+            int maxScore = 0;
+
+            foreach (var student in studentExams)
+            {
+                if (student.Value.Values.Sum() > maxScore)
+                {
+                    maxScore = student.Value.Values.Sum();
+                    bestStudent = student.Key;
+                }
+            }
+
+            Console.WriteLine($"Best candidate is {bestStudent} with total {maxScore} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var student in studentExams.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(student.Key);
+
+                foreach (var exam in student.Value.OrderByDescending(p => p.Value))
+                {
+                    Console.WriteLine($"#  {exam.Key} -> {exam.Value}");
+                }
+            }
+        }
+    }
+}

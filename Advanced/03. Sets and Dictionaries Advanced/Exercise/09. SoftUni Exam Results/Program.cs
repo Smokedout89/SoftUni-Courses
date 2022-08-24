e60eaf1218a7be10440bbabfc25986f7efedb943
Smokedout89 <smokedout8989@gmail.com>
2022-08-24 20:56:02 +0300

    Uploading new files

---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09.__SoftUni_Exam_Results
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Dictionary<string, int> submissions = new Dictionary<string, int>();
+            Dictionary<string, int> students = new Dictionary<string, int>();
+
+            while ((command = Console.ReadLine()) != "exam finished")
+            {
+                string[] cmdArgs = command.Split('-');
+                string name = cmdArgs[0];
+                string language = cmdArgs[1];
+
+                if (language == "banned")
+                {
+                    students.Remove(name);
+                    continue;
+                }
+
+                int points = int.Parse(cmdArgs[2]);
+
+                if (!submissions.ContainsKey(language))
+                {
+                    submissions.Add(language, 0);
+                }
+
+                submissions[language]++;
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, points);
+                }
+
+                if (students[name] < points)
+                {
+                    students[name] = points;
+                }
+            }
+
+            Dictionary<string, int> orderedStudents = students
+                .OrderByDescending(p => p.Value)
+                .ThenBy(n => n.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine("Results:");
+            foreach (var student in orderedStudents)
+            {
+                Console.WriteLine($"{student.Key} | {student.Value}");
+            }
+
+            Dictionary<string, int> sortedBySubmissions = submissions
+                .OrderByDescending(c => c.Value)
+                .ThenBy(s => s.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine("Submissions:");
+            foreach (var submission in sortedBySubmissions)
+            {
+                Console.WriteLine($"{submission.Key} - {submission.Value}");
+            }
+        }
+    }
+}
---
+++ Advanced/03. Sets and Dictionaries Advanced/Exercise/10. ForceBook/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10._ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> forceRegister = new Dictionary<string, List<string>>();
+
+            while (input != "Lumpawaroo")
+            {
+                if (input.Contains('|'))
+                {
+                    string[] data = input.Split(new string[] { " | " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string side = data[0];
+                    string user = data[1];
+
+                    if (forceRegister.ContainsKey(side) == false)
+                    {
+                        forceRegister.Add(side, new List<string>());
+                    }
+                    if (forceRegister[side].Contains(user) == false && forceRegister.Values.Any(x => x.Contains(user)) == false)
+                    {
+                        forceRegister[side].Add(user);
+                    }
+                }
+                else if (input.Contains("->"))
+                {
+                    string[] data = input.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string user = data[0];
+                    string side = data[1];
+
+                    if (forceRegister.Any(x => x.Value.Contains(user)) == false)
+                    {
+                        if (forceRegister.ContainsKey(side) == false)
+                        {
+                            forceRegister.Add(side, new List<String>());
+                        }
+
+                        forceRegister[side].Add(user);
+                        Console.WriteLine($"{user} joins the {side} side!");
+                    }
+                    else
+                    {
+                        foreach (var s in forceRegister)
+                        {
+                            if (s.Value.Contains(user))
+                            {
+                                s.Value.Remove(user);
+                            }
+                        }
+
+                        if (forceRegister.ContainsKey(side) == false)
+                        {
+                            forceRegister.Add(side, new List<string>());
+                        }
+                        forceRegister[side].Add(user);
+                        Console.WriteLine($"{user} joins the {side} side!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            foreach (var side in forceRegister.Where(x => x.Value.Count > 0).OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count()}");
+                foreach (var user in side.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+        }
+    }
+}

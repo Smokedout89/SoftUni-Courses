631d93abfb5a1e56779dbb20882dd8c5853a6085
Smokedout89 <smokedout8989@gmail.com>
2022-08-26 10:43:15 +0300

    Uploading new files

---
+++ Advanced/04. Streams Files and Dictionaries/Lab/MergeFiles/MergeFiles.cs
@@ -0,0 +1,39 @@
+﻿
+namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            List<string> mergedList = new List<string>();
+
+            string[] firstFileLines = File.ReadAllLines(firstInputFilePath);
+            string[] secondFileLines = File.ReadAllLines(secondInputFilePath);
+
+            foreach (string line in firstFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            foreach (string line in secondFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            File.WriteAllLines(outputFilePath, mergedList.OrderBy(x => x));
+        }
+    }
+}
---
+++ Advanced/04. Streams Files and Dictionaries/Lab/OddLines/OddLines.cs
@@ -0,0 +1,38 @@
+﻿namespace OddLines
+{
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(@"..\..\..\Files\input.txt");
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\Files\output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}

22f0a6a428af44d3f846db85be0d147bab79df33
Smokedout89 <smokedout8989@gmail.com>
2022-08-29 10:44:09 +0300

    Uploading new files

---
+++ Advanced/04. Streams Files and Dictionaries/Exercise/CopyBinaryFile/CopyBinaryFiles.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream readear = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int countBytes = readear.Read(buffer, 0, buffer.Length);
+
+                        if (countBytes == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, countBytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/04. Streams Files and Dictionaries/Exercise/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DirectoryTraversal
+{
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            string[] files = Directory.GetFiles(inputFolderPath);
+            Dictionary<string, List<FileInfo>> extensionsInfo = new Dictionary<string, List<FileInfo>>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string extension = fileInfo.Extension;
+
+                if (!extensionsInfo.ContainsKey(extension))
+                {
+                    extensionsInfo.Add(extension, new List<FileInfo>());
+                }
+
+                extensionsInfo[extension].Add(fileInfo);
+            }
+
+            foreach (var entry in extensionsInfo.OrderByDescending(file => file.Value.Count).ThenBy(extension => extension.Key))
+            {
+                string extension = entry.Key;
+                sb.AppendLine(extension);
+                List<FileInfo> filesInfo = entry.Value;
+
+                foreach (FileInfo fileInfo in filesInfo.OrderByDescending(file => file.Length))
+                {
+                    sb.AppendLine($"--{fileInfo.Name} - {fileInfo.Length / 1024:f3}kb");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string pathReport = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            File.WriteAllText(pathReport, textContent);
+        }
+    }
+}

2cbfa96e938c1e8c82add3cbf89a348ad5a538b2
Smokedout89 <smokedout8989@gmail.com>
2022-08-30 10:33:11 +0300

    Uploading new files

---
+++ Advanced/04. Streams Files and Dictionaries/Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilepath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilepath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 1;
+            List<string> modifiedLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                int letterCount = line.Count(char.IsLetter);
+                int punctionCount = line.Count(char.IsPunctuation);
+
+                string modifiedLine = $"Line {count}: {line} ({letterCount})({punctionCount})";
+                modifiedLines.Add(modifiedLine);
+                count++;
+            }
+
+            File.WriteAllLines(outputFilePath, modifiedLines);
+        }
+    }
+}

692f523f78c4648c6614bbbe04ad987209ddd39a
Smokedout89 <smokedout8989@gmail.com>
2022-08-31 10:19:45 +0300

    Uploading new files

---
+++ Advanced/05. Functional Programming/Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ Advanced/05. Functional Programming/Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Func<string, bool> checker = w => char.IsUpper(w[0]);
+            Predicate<string> checker = w => char.IsUpper(w[0]);
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => checker(w))
+                .ToArray();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

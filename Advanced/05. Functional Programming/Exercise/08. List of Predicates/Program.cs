ad3bd457e0cd4299fb0449a2e17f84785042901f
Smokedout89 <smokedout8989@gmail.com>
2022-09-05 10:26:05 +0300

    Uploading new files

---
+++ Advanced/05. Functional Programming/Exercise/08. List of Predicates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._List_of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = Enumerable.Range(1, n).ToList(); 
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            foreach (var divider in dividers)
+            {
+                predicates.Add(x => x % divider == 0);
+            }
+
+            foreach (var number in numbers)
+            {
+                bool isDivisible = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/05. Functional Programming/Exercise/09. Predicate Party!/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09._Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            string command = Console.ReadLine();
+
+            while (command != "Party!")
+            {
+                string[] cmdArgs = command.Split();
+                string cmd = cmdArgs[0];
+                string operation = cmdArgs[1];
+                string value = cmdArgs[2];
+
+                if (cmd == "Double")
+                {
+                   List<string> doubleNames = names.FindAll(GetPredicate(operation, value));
+
+                   if (!doubleNames.Any())
+                   {
+                       command = Console.ReadLine();
+                       continue;
+                   }
+
+                   int index = names.FindIndex(GetPredicate(operation, value));
+                   names.InsertRange(index, doubleNames);
+                }
+                else
+                {
+                    names.RemoveAll(GetPredicate(operation, value));
+                }
+
+                command = Console.ReadLine(); 
+            }
+
+            if (names.Any())
+            {
+                Console.WriteLine($"{string.Join(", ", names)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        private static Predicate<string> GetPredicate(string operation, string value)
+        {
+            if (operation == "StartsWith")
+            {
+                return x => x.StartsWith(value);
+            }
+
+            if (operation == "EndsWith")
+            {
+                return x => x.EndsWith(value);
+            }
+
+            int valueAsInt = int.Parse(value);
+
+            return x => x.Length == valueAsInt;
+        }
+    }
+}

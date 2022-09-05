da00da1aac690b181b47555fbb356a861f45dd0c
Smokedout89 <smokedout8989@gmail.com>
2022-09-05 13:21:12 +0300

    Uploading new files

---
+++ Advanced/05. Functional Programming/Exercise/10. Party Reservation Filter Module/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10._Party_Reservation_Filter_Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            string command = Console.ReadLine();
+
+            Dictionary<string, Predicate<string>> allFilters = new Dictionary<string, Predicate<string>>();
+
+            while (command != "Print")
+            {
+                string[] cmdArgs = command.Split(';');
+                string cmd = cmdArgs[0];
+                string operation = cmdArgs[1];
+                string value = cmdArgs[2];
+
+                if (cmd == "Add filter")
+                {
+                    allFilters.Add(operation + value, GetPredicate(operation, value));
+                }
+                else
+                {
+                    allFilters.Remove(operation + value);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var (key, value) in allFilters)
+            {
+                names.RemoveAll(value);
+            }
+
+            Console.WriteLine(string.Join(' ', names));
+        }
+
+        private static Predicate<string> GetPredicate(string operation, string value)
+        {
+            if (operation == "Starts with")
+            {
+                return x => x.StartsWith(value);
+            }
+
+            if (operation == "Ends with")
+            {
+                return x => x.EndsWith(value);
+            }
+
+            if (operation == "Contains")
+            {
+                return x => x.Contains(value);
+            }
+
+            int valueAsInt = int.Parse(value);
+
+            return x => x.Length == valueAsInt;
+        }
+    }
+}
---
+++ Advanced/05. Functional Programming/Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int asciiSum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isLargerOrEqual = (name, sum) => name.Sum(x => x) >= sum;
+
+            Func<string[], int, Func<string, int, bool>, string> getName = (name, sum, func) =>
+                names.Where(x => func(x, sum)).FirstOrDefault();
+
+            string name = getName(names, asciiSum, isLargerOrEqual);
+
+            Console.WriteLine(name);
+        }
+    }
+}

8aad480f00ecca2df9d69d7ebd8637d26ef059cd
Smokedout89 <smokedout8989@gmail.com>
2022-09-09 10:53:00 +0300

    Uploading new files

---
+++ Advanced/07. Implementing LL & SQ/LinkedList/LinkedList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class CustomDoublyLinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public Node<T> Tail { get; set; }
+
+        public void ForEach(Action<Node<T>> action)
+        {
+            var node = Head;
+
+            while (node != null)
+            {
+                action(node);
+                node = node.Next;
+            }
+        }
+
+        public Node<T>[] ToArray()
+        {
+            List<Node<T>> list = new List<Node<T>>();
+            var node = Head;
+
+            while (node != null)
+            {
+                list.Add(node);
+                node = node.Next;
+            }
+
+            return list.ToArray();
+        }
+
+        public void AddFirst(Node<T> node)
+        {
+            if (!CheckIfFirstElement(node))
+            {
+                Node<T> previousHead = Head;
+                Head = node;
+                previousHead.Previous = Head;
+                Head.Next = previousHead;
+            }
+        }
+
+        public void AddLast(Node<T> node)
+        {
+            if (!CheckIfFirstElement(node))
+            {
+                Node<T> previousTail = Tail;
+                Tail = node;
+                previousTail.Next = Tail;
+                Tail.Previous = previousTail;
+            }
+        }
+
+        public Node<T> RemoveFirst()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+
+            var previous = Head;
+            var next = Head.Next;
+
+            if (next != null)
+            {
+                next.Previous = null;
+            }
+            else
+            {
+                Tail = null;
+            }
+            Head = next;
+
+            return previous;
+        }
+
+        public Node<T> RemoveLast()
+        {
+            if (Tail == null)
+            {
+                return null;
+            }
+
+            var previous = Tail;
+            var next = Tail.Previous;
+
+            if (next != null)
+            {
+                next.Next = null;
+            }
+            else
+            {
+                Head = null;
+            }
+            Tail = next;
+
+            return previous;
+        }
+
+        private bool CheckIfFirstElement(Node<T> node)
+        {
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Advanced/07. Implementing LL & SQ/LinkedList/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomDoublyLinkedList<int> list = new CustomDoublyLinkedList<int>();
+
+            list.AddFirst(new Node<int>(1));
+            list.AddFirst(new Node<int>(2));
+            list.AddFirst(new Node<int>(3));
+            list.AddLast(new Node<int>(1));
+            list.AddLast(new Node<int>(2));
+            list.AddLast(new Node<int>(3));
+
+            list.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+
+            Console.WriteLine($"Removed First: {list.RemoveFirst().Value}");
+            Console.WriteLine($"Removed First: {list.RemoveFirst().Value}");
+
+            Console.WriteLine($"Removed Last: {list.RemoveLast().Value}");
+            Console.WriteLine($"Removed Last: {list.RemoveLast().Value}");
+
+            Node<int> node = list.Head;
+
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }   
+        }
+    }
+}

020f17f1d8ca129d740f539c6b3776fb9727a1da
Smokedout89 <smokedout8989@gmail.com>
2022-09-29 11:05:35 +0300

    Uploading new files

---
+++ OOP/01. Inheritance/Exercise/Restaurant/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const int cakeGrams = 250;
+        private const int cakeCalories = 1000;
+        private const int cakePrice = 5;
+        public Cake(string name) : base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+        }
+    }
+}

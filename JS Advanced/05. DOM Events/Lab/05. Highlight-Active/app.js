13d7193bdcfec9ade8c051fc67d3773d28d8fb85
Smokedout89 <smokedout8989@gmail.com>
2022-11-22 09:57:33 +0200

    Uploading new files

---
+++ JS Advanced/05. DOM Events/Lab/05. Highlight-Active/app.js
@@ -0,0 +1,16 @@
+function focused() {
+    Array.from(document.querySelectorAll('input'))
+        .forEach(i => {
+            i.addEventListener('focus', onFocus);
+            i.addEventListener('blur', onBlur);
+        });
+
+
+    function onFocus(event) {
+        event.target.parentElement.className = 'focused';
+    }
+
+    function onBlur(event) {
+        event.target.parentElement.className = '';
+    }
+}

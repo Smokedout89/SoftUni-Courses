d8f988fded60c0f49235a4c446c9612877a9e2e8
Smokedout89 <smokedout8989@gmail.com>
2023-01-03 15:55:37 +0200

    Uploading new files

---
+++ JS Advanced/09. Prototypes and Inheritance/Exercise/06. Posts.js
@@ -0,0 +1,86 @@
+function posts() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let superString = super.toString();
+            let rating = this.likes - this.dislikes;
+
+            if (this.comments.length > 0) {
+                let commentsText = '';
+                for (let i = 0; i < this.comments.length; i++) {
+                    commentsText += `\n * ${this.comments[i]}`;
+                }
+                return `${superString}\nRating: ${rating}\nComments:${commentsText}`;
+            } else {
+                return `${superString}\nRating: ${rating}`;
+            }
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost
+    }
+}
+
+const classes = posts();
+let post = new classes.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!

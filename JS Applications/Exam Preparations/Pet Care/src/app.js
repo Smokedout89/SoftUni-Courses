7efcaebc3cee31d45dbfb54cf974335493485317
Smokedout89 <smokedout8989@gmail.com>
2023-01-23 10:57:43 +0200

    Uploading new files

---
+++ JS Applications/Exam Preparations/Pet Care/src/app.js
@@ -0,0 +1,41 @@
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { catalogView } from './views/catalog.js';
+import { createView } from './views/create.js';
+import { detailsView } from './views/details.js';
+import { editView } from './views/edit.js';
+import { homeView } from './views/home.js';
+import { loginView } from './views/login.js';
+import { updateNav } from './views/nav.js';
+import { registerView } from './views/register.js';
+
+const main = document.getElementById('content');
+
+page(decorateContext);
+page('/', homeView);
+page('/catalog', catalogView);
+page('/catalog/:id', detailsView);
+page('/edit/:id', editView);
+page('/login', loginView);
+page('/register', registerView);
+page('/create', createView);
+
+updateNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    const user = getUserData();
+
+    if (user) {
+        ctx.user = user;
+    }
+
+    next();
+}
+
+function renderMain(content) {
+    render(content, main);
+}

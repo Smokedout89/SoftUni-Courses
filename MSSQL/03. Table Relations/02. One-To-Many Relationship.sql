b1222e8659214a20d58ba70437d9a2768d387e15
Smokedout89 <smokedout8989@gmail.com>
2023-02-13 20:15:47 +0200

    Uploading new files

---
+++ MSSQL/03. Table Relations/02. One-To-Many Relationship.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Manufacturers(
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models(
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(30) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID) NOT NULL
+)

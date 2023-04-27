d38334889a8f459227b4d8d4ca0cdbb2e0c7dea6
Smokedout89 <smokedout8989@gmail.com>
2023-04-27 23:26:55 +0300

    Uploading new files

---
+++ Algorithms/01. Recursion And Backtracking/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.Recursive_Array_Sum
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(SumRecursively(numbers, 0));
+        }
+
+        private static int SumRecursively(int[] numbers, int idx)
+        {
+            if (idx == numbers.Length - 1)
+            {
+                return numbers[idx];
+            }
+
+            return numbers[idx] + SumRecursively(numbers, idx + 1);
+        }
+    }
+}
---
+++ Algorithms/01. Recursion And Backtracking/02. Recursive Drawing/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.Recursive_Drawing
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int row)
+        {
+            if (row == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', row));
+
+            DrawFigure(row - 1);
+
+            Console.WriteLine(new string('#', row));
+        }
+    }
+}

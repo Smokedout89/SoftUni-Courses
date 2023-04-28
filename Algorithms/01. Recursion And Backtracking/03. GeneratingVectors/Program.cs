38dd116909429346d995a85bb46435959e8fd9f2
Smokedout89 <smokedout8989@gmail.com>
2023-04-28 19:19:47 +0300

    Uploading new files

---
+++ Algorithms/01. Recursion And Backtracking/03. GeneratingVectors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.GeneratingVectors
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Generate01(arr, 0);
+        }
+
+        private static void Generate01(int[] arr, int idx) 
+        {
+            if (idx >= arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[idx] = i;
+
+                Generate01(arr, idx + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms/01. Recursion And Backtracking/04. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04.Recursive_Factorial
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorialRecursively(n));
+        }
+
+        private static int GetFactorialRecursively(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * GetFactorialRecursively(n - 1);
+        }
+    }
+}

f9d1c85470d0907884a240b310e3bf4f8e733c68
Smokedout89 <smokedout8989@gmail.com>
2023-05-05 19:29:09 +0300

    Uploading new files

---
+++ Algorithms/02. Combinatorial Problems/01. Permutations without Repetitions/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _01.Permutations_without_Repetitions
+{
+    using System;
+
+    internal class Program
+    {
+        private static string[] elements;
+
+        //private static string[] permutations;
+        //private static bool[] used;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            //permutations = new string[elements.Length];
+            //used = new bool[elements.Length];
+
+            GeneratePermutation(0);
+        }
+
+        private static void GeneratePermutation(int idx)
+        {
+            if (idx >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            GeneratePermutation(idx + 1);
+
+            for (int i = idx + 1; i < elements.Length; i++)
+            {
+                Swap(idx, i);
+                GeneratePermutation(idx + 1);
+                Swap(idx, i);
+            }
+
+            //if (idx >= permutations.Length)
+            //{
+            //    Console.WriteLine(string.Join(" ", permutations));
+            //    return;
+            //}
+
+            //for (int i = 0; i < elements.Length; i++)
+            //{
+            //    if (!used[i])
+            //    {
+            //        used[i] = true;
+            //        permutations[idx] = elements[i];
+
+            //        GeneratePermutation(idx + 1);
+
+            //        used[i] = false;
+            //    }
+
+            //}
+        }
+
+        private static void Swap(int first, int second)
+        {
+            // (elements[first], elements[second]) = (elements[second], elements[first]);
+
+            string temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
---
+++ Algorithms/02. Combinatorial Problems/02. Permutations with Repetitions/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _02.Permutations_with_Repetitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static string[] elements;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            GeneratePermutation(0);
+        }
+
+        private static void GeneratePermutation(int idx)
+        {
+            if (idx >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            GeneratePermutation(idx + 1);
+
+            HashSet<string> used = new HashSet<string> { elements[idx] };
+
+            for (int i = idx + 1; i < elements.Length; i++)
+            {
+                if (!used.Contains(elements[i]))
+                {
+                    Swap(idx, i);
+                    GeneratePermutation(idx + 1);
+                    Swap(idx, i);
+
+                    used.Add(elements[i]);
+                }             
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            // (elements[first], elements[second]) = (elements[second], elements[first]);
+
+            string temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}

314028ef260d46d016c000e371a0d480bcc075e0
Smokedout89 <smokedout8989@gmail.com>
2023-05-12 19:28:10 +0300

    Uploading new files

---
+++ Algorithms/02. Combinatorial Problems/04. Variations with Repetition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04.Variations_with_Repetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            GenerateVariations(0);
+        }
+
+        private static void GenerateVariations(int idx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[idx] = elements[i];
+                GenerateVariations(idx + 1);
+            }
+        }
+    }
+}

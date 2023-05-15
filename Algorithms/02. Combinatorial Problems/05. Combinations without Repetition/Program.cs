8bc96509036195e3ca10d0dc289599cf11e5d794
Smokedout89 <smokedout8989@gmail.com>
2023-05-15 23:10:10 +0300

    Uploading new files

---
+++ Algorithms/02. Combinatorial Problems/05. Combinations without Repetition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.Combinations_without_Repetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            GenerateCombinations(0, 0);
+        }
+
+        private static void GenerateCombinations(int idx, int elementStartIdx)
+        {
+            if (idx >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[idx] = elements[i];
+                GenerateCombinations(idx + 1, i + 1);
+            }
+        }
+    }
+}

78231c743fd54e86f39973d94322017d219bde51
Smokedout89 <smokedout8989@gmail.com>
2023-05-16 13:50:31 +0300

    Uploading new files

---
+++ Algorithms/02. Combinatorial Problems/07. N Choose K Count/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.N_Choose_K_Count
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            return GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+        }
+    }
+}

e843e51b253deb88213e7b151df13c77d83c863e
Smokedout89 <smokedout8989@gmail.com>
2023-05-17 17:47:44 +0300

    Uploading new files

---
+++ Algorithms/03. RecursionAndCombinatorialExericse/01. Reverse Array/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.Reverse_Array
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int[] numbers;
+
+        static void Main(string[] args)
+        {
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            ReverseArray(0);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void ReverseArray(int idx)
+        {
+            if (idx == numbers.Length / 2)
+            {
+                return;
+            }
+
+            int temp = numbers[idx];
+            numbers[idx] = numbers[numbers.Length - idx - 1];
+            numbers[numbers.Length - idx - 1] = temp;
+
+            ReverseArray(idx + 1);
+        }
+    }
+}
---
+++ Algorithms/03. RecursionAndCombinatorialExericse/02. Nested Loops To Recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.Nested_Loops_To_Recursion
+{
+    using System;
+
+    internal class Program
+    {
+        private static int[] numbers;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            numbers = new int[n];
+
+            GenerateLoops(0);
+        }
+
+        private static void GenerateLoops(int idx)
+        {
+            if (idx >= numbers.Length)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+                return;
+            }
+
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                numbers[idx] = i;
+
+                GenerateLoops(idx + 1);
+            }
+        }
+    }
+}

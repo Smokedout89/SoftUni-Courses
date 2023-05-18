5959f3d4cf6af6ca0d730cf5a563bd585c7c3016
Smokedout89 <smokedout8989@gmail.com>
2023-05-18 19:51:20 +0300

    Uploading new files

---
+++ Algorithms/03. RecursionAndCombinatorialExericse/03. Connected Areas in a Matrix/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace _03.Connected_Areas_in_a_Matrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static char[,] matrix;
+        private static int size;
+        private const char VisitedSymbol = 'v';
+
+        public class Area
+        {
+            public int Row { get; set; }
+
+            public int Col { get; set; }
+
+            public int Size { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            matrix = new char[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                string elements = Console.ReadLine();
+
+                for (int c = 0; c < cols; c++)
+                {
+                    matrix[r, c] = elements[c];
+                }
+            }
+
+            var areas = new List<Area>();
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    size = 0;
+                    ExploreArea(r, c);
+
+                    if (size > 0)
+                    {
+                        areas.Add(new Area { Row = r, Col = c, Size = size });
+                    }
+                }
+            }
+
+            var sorted = areas
+                .OrderByDescending(s => s.Size)
+                .ThenBy(r => r.Row)
+                .ThenBy(c => c.Col)
+                .ToList();
+
+            Console.WriteLine($"Total areas found: {areas.Count}");
+
+            for (int i = 0; i < areas.Count; i++)
+            {
+                var area = sorted[i];
+                Console.WriteLine($"Area #{i + 1} at ({area.Row}, {area.Col}), size: {area.Size}");
+            }
+        }
+
+        private static void ExploreArea(int row, int col)
+        {
+            if (IsOutside(row, col) || IsWall(row, col) || IsVisited(row, col))
+            {
+                return;
+            }
+
+            size += 1;
+            matrix[row, col] = VisitedSymbol;
+
+            ExploreArea(row - 1, col);
+            ExploreArea(row + 1, col);
+            ExploreArea(row, col - 1);
+            ExploreArea(row, col + 1);
+        }
+
+        private static bool IsVisited(int row, int col)
+        {
+            return matrix[row, col] == VisitedSymbol;
+        }
+
+        private static bool IsWall(int row, int col)
+        {
+            return matrix[row, col] == '*';
+        }
+
+        private static bool IsOutside(int row, int col)
+        {
+            return row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1);
+        }
+    }
+}
---
+++ Algorithms/03. RecursionAndCombinatorialExericse/04. Cinema/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _04.Cinema
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static List<string> nonStaticPeople;
+        private static string[] people;
+        private static bool[] lockedPositions;
+
+        static void Main(string[] args)
+        {
+            nonStaticPeople = Console.ReadLine().Split(", ").ToList();
+            people = new string[nonStaticPeople.Count];
+            lockedPositions = new bool[nonStaticPeople.Count];
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "generate")
+                {
+                    break;
+                }
+
+                string[] inputParts = input.Split(" - ");
+                string name = inputParts[0];
+                int position = int.Parse(inputParts[1]) - 1;
+
+                people[position] = name;
+                lockedPositions[position] = true;
+
+                nonStaticPeople.Remove(name);
+            }
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= nonStaticPeople.Count)
+            {
+                PrintPermutation();
+                return;
+            }
+
+            Permute(idx + 1);
+
+            for (int i = idx + 1; i < nonStaticPeople.Count; i++)
+            {
+                Swap(idx, i);
+                Permute(idx + 1);
+                Swap(idx, i);
+            }
+        }
+
+        private static void PrintPermutation()
+        {
+            int nonStaticIdx = 0;
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (!lockedPositions[i])
+                {
+                    people[i] = nonStaticPeople[nonStaticIdx++];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", people));
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = nonStaticPeople[first];
+            nonStaticPeople[first] = nonStaticPeople[second];
+            nonStaticPeople[second] = temp;
+        }
+    }
+}

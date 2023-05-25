98517e5784fa22628896394dfecc49818d232c26
Smokedout89 <smokedout8989@gmail.com>
2023-05-25 17:43:21 +0300

    Uploading new files

---
+++ Algorithms/04. Searching, Sorting and Greedy Algorithms/01. Binary Search/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+namespace _01._Binary_Search
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numberToFindIndex = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, numberToFindIndex));
+        }
+
+        private static int BinarySearch(int[] numbers, int numberToFindIndex)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+
+                if (numbers[mid] == numberToFindIndex)
+                {
+                    return mid;
+                }
+
+                if (numberToFindIndex > numbers[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Algorithms/04. Searching, Sorting and Greedy Algorithms/02. Selection Sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace _02._Selection_Sort
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SelectionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var min = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(numbers, i, min);
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
